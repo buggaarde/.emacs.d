bf64d5683a1082be89bf4ba23c5979d34425c0ce
Simon Bugge Siggaard <simsig+githubOfficial@gmail.com>
2019-04-22 15:59:10 +0200

    Added custom.el

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist '((fullscreen . maximized)))
+ '(package-selected-packages
+   '(esup writeroom-mode writegood-mode use-package spaceline py-yapf py-autopep8 ox-pandoc ox-gfm objed markdown-mode magit jedi importmagic go-guru go-complete flycheck-golangci-lint flx expand-region exec-path-from-shell elpy dockerfile-mode docker-compose-mode diminish counsel company-go comment-dwim-2 clj-refactor ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
