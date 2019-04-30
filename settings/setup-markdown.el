a0302085e10cf5a1a2b97093a97d689551fa9dd4
Simon Bugge Siggaard <simsig+githubOfficial@gmail.com>
2019-04-30 15:41:28 +0200

    Activate visual-line-mode in markdown-mode

--- settings/setup-markdown.el
+++ settings/setup-markdown.el
@@ -5,6 +5,8 @@
   :mode (("README\\.md\\'" . gfm-mode)
 		 ("\\.md\\'" . markdown-mode)
 		 ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+  :init (setq markdown-command "multimarkdown")
+
+  :hook ((markdown-mode . (lambda () (visual-line-mode)))))
 
 (provide 'setup-markdown)
