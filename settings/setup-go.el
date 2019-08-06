3ff5d9b9fe7b7969849ee91b81d6e67119517159
Simon Bugge Siggaard <buggaarde@gmail.com>
2019-08-06 11:18:40 +0200

    Add linters to go-mode

--- settings/setup-go.el
+++ settings/setup-go.el
@@ -7,11 +7,16 @@
 
   :config
   (add-to-list 'exec-path (concat (getenv "GOPATH") "/src/github.com/nsf/gocode"))
+  
   (setq gofmt-command (concat (getenv "GOPATH") "/bin/goimports"))
   (setq godef-command (concat (getenv "GOPATH") "/bin/godef"))
+  ;; (setq godef-command (concat (getenv "GOPATH") "/bin/misspell"))
+  ;; (setq godef-command (concat (getenv "GOPATH") "/bin/golint"))
 
   (add-hook 'go-mode-hook '(lambda ()
 							 (add-hook 'before-save-hook 'gofmt-before-save)))
+  (add-hook 'flycheck-mode-hook '(lambda ()
+								   (add-to-list 'flycheck-checkers 'go-golint)))
 
   (use-package go-autocomplete
 	:load-path  "/Users/simonbuggesiggaard/go/src/github.com/nsf/gocode/emacs")
@@ -23,7 +28,6 @@
 
 (use-package flycheck-golangci-lint
   :ensure t
-  :defer t
   :hook (go-mode . flycheck-golangci-lint-setup))
 
 ;; define custom functions for option-snippet
