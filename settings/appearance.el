de04e544ebd23bc6065bb2be45adaf2ee52ba633
Simon Bugge Siggaard <buggaarde@gmail.com>
2019-08-06 08:58:32 +0200

    Add spacemacs-light-theme and easy access to different fonts

--- settings/appearance.el
+++ settings/appearance.el
@@ -1,17 +1,23 @@
-;; Set color theme
-(set-background-color "black")
-(set-foreground-color "white")
-(set-face-background 'region "#464740")
-(set-face-background 'highlight "#111111")
-(set-face-background 'show-paren-match "#333399")
-(set-face-background 'show-paren-mismatch "red")
+;; ;; Set color theme
+;; (set-background-color "black")
+;; (set-foreground-color "white")
+;; (set-face-background 'region "#464740")
+;; (set-face-background 'highlight "#111111")
+;; (set-face-background 'show-paren-match "#333399")
+;; (set-face-background 'show-paren-mismatch "red")
+
+(use-package spacemacs-common
+  :ensure spacemacs-theme
+  :config (load-theme 'spacemacs-light t))
 
 ;; Set fonts
-(set-face-attribute 'default nil :font "Monaco" :height 150)
-;; (require 'fira-code-mode)
-;; (use-package fira-code-mode
-;;   :diminish fira-code-mode
-;;   :hook (prog-mode . fira-code-mode))
+;; (set-face-attribute 'default nil :font "Monaco" :height 140)
+(require 'fira-code-mode)
+(set-face-attribute 'default nil :font "Fira Code" :height 140)
+(use-package fira-code-mode
+  :diminish fira-code-mode
+  :hook (prog-mode . fira-code-mode))
+;; (set-face-attribute 'default nil :font "Bookerly" :height 150)
 
 ;; Load Powerline
 (require 'setup-powerline)
