daf5211d68b94ad827bae4e1ea5ea4b954b0e546
Simon Bugge Siggaard <buggaarde@gmail.com>
2020-03-02 09:26:56 +0100

    setup haskell-mode

---
+++ settings/setup-haskell.el
@@ -0,0 +1,15 @@
+(use-package haskell-mode
+  :straight (haskell-mode :type git :host github :repo "haskell/haskell-mode")
+
+  :config
+  (use-package lsp-haskell
+	:straight (lsp-haskell :type git :host github :repo "emacs-lsp/lsp-haskell"))
+
+  (require 'lsp)
+  (require 'lsp-haskell)
+  (add-hook 'haskell-mode-hook #'lsp)
+  
+  (setq haskell-stylish-on-save t)
+  (setq haskell-mode-stylish-haskell-path "brittany"))
+
+(provide 'setup-haskell)
