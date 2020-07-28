1389d8caf9e8f9efadef0ab8402cbe89cc5ce20a
Simon Bugge Siggaard <buggaarde@gmail.com>
2020-07-28 12:23:29 +0200

    use doom-tomorrow-night-theme

--- settings/appearance.el
+++ settings/appearance.el
@@ -1,30 +1,32 @@
 ;; -*- lexical-binding: t; -*-
 
-;; (use-package doom-themes
-;;   :straight t
+;; (use-package white-sand-theme
+;;   :straight (white-sand-theme
+;; 			 :type git :host github
+;; 			 :repo "mswift42/white-sand-theme")
+;;   :config (load-theme 'white-sand))
 
-;;   :config
-;;   (setq doom-themes-enable-bold t
-;; 		doom-themes-enable-italic t)
 
-;;   (load-theme 'doom-nord))
-
-(use-package white-sand-theme
-  :straight (white-sand-theme
-			 :type git :host github
-			 :repo "mswift42/white-sand-theme")
-  :config (load-theme 'white-sand))
+(use-package doom-themes
+  :straight t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-tomorrow-night t)
+  
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 ;; Set fonts
 ;; (set-face-attribute 'default nil :font "Source Code Pro" :height 150)
-(require 'fira-code-mode)
-(set-face-attribute 'default nil :font "Fira Code" :height 150)
-(use-package fira-code-mode
-  :diminish fira-code-mode
-  :hook (prog-mode . fira-code-mode))
-;; (set-face-attribute 'default nil :font "Bookerly" :height 150)
-
-;; Load Powerline
-;; (require 'setup-powerline)
+(set-face-attribute 'default nil :font "JetBrains Mono" :height 130)
+(require 'jetbrains-mono-mode)
+(add-hook 'prog-mode-hook jetbrains-ligature-mode)
+;; (require 'fira-code-mode)
+;; (set-face-attribute 'default nil :font "Fira Code" :height 150)
+;; (use-package fira-code-mode
+;;   :diminish fira-code-mode
+;;   :hook (prog-mode . fira-code-mode))
 
 (provide 'appearance)
