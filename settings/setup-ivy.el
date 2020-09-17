4cb4af2d931f8aba7b8eb67d359c2a7ffd3078c7
Simon Bugge Siggaard <buggaarde@gmail.com>
2020-09-17 13:54:38 +0200

    don't use ivy-posframe

--- settings/setup-ivy.el
+++ settings/setup-ivy.el
@@ -22,16 +22,17 @@
 		   ("C-x C-f" . counsel-find-file)
 		   ("C-c C-r" . ivy-resume))))
 
-(use-package ivy-posframe
-  :straight (ivy-posframe
-			 :type git :host github
-			 :repo "tumashu/ivy-posframe")
-  :init
-  (setq ivy-posframe-display-functions-alist
-		'((swiper            . ivy-display-function-fallback)
-		  (counsel-find-file . ivy-posframe-display-at-frame-top-center)
-		  (ivy-switch-buffer . ivy-posframe-display-at-frame-top-center)
-          (counsel-M-x       . ivy-posframe-display-at-frame-top-center)))
-  (ivy-posframe-mode 1))
+;; (use-package ivy-posframe
+;;   :straight (ivy-posframe
+;; 			 :type git :host github
+;; 			 :repo "tumashu/ivy-posframe")
+;;   :init
+;;   (setq ivy-posframe-display-functions-alist
+;; 		'((swiper            . ivy-display-function-fallback)
+;; 		  (counsel-find-file . ivy-posframe-display-at-frame-top-center)
+;; 		  (ivy-switch-buffer . ivy-posframe-display-at-frame-top-center)
+;;           (counsel-M-x       . ivy-posframe-display-at-frame-top-center)))
+;;   (ivy-posframe-mode 1)
+;;  )										;
 
 (provide 'setup-ivy)
